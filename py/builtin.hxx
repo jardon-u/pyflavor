4f7cbcaf51a653c4999d5383b0603e1b21b3f34c
Ugo Jardonnet <u.jardonnet@criteo.com>
2014-07-30 16:02:51 +0200

    remove zip based on boost::range

--- py/builtin.hxx
+++ py/builtin.hxx
@@ -2,7 +2,6 @@
 #include <tuple>
 #include <algorithm>
 #include <boost/iterator/zip_iterator.hpp>
-#include <boost/range.hpp>
 #include <iostream>
 
 namespace py
@@ -18,16 +17,5 @@ namespace py
   {
     std::cout << value << " ";
     print(args...);
-  }
-
-  // Zip N containers
-  template <typename... T>
-  inline
-  auto zip(const T&... containers)
-    -> boost::iterator_range<boost::zip_iterator<decltype(boost::make_tuple(std::begin(containers)...))>>
-  {
-    auto zip_begin = boost::make_zip_iterator(boost::make_tuple(std::begin(containers)...));
-    auto zip_end = boost::make_zip_iterator(boost::make_tuple(std::end(containers)...));
-    return boost::make_iterator_range(zip_begin, zip_end);
   }
 }
